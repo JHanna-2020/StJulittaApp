fb232c96a00843514b3f64564ebbeb4453480a3f
JHanna-2020 <66224091+JHanna-2020@users.noreply.github.com>
2026-03-06 17:35:32 -0600

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by John Hanna on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTests/AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppTests.swift
+//  AppTests
+//
+//  Created by John Hanna on 3/6/26.
+//
+
+import Testing
+@testable import App
+
+struct AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
